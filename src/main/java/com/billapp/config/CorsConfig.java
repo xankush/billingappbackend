ea460f49d9011513461f9b3f6c70d096f507aa6b
xankush <ankushdohare12@gmail.com>
2026-01-28 12:38:11 +0530

    allow the cors localhos 8080

--- src/main/java/com/billapp/config/CorsConfig.java
+++ src/main/java/com/billapp/config/CorsConfig.java
@@ -1,25 +1,22 @@
-// src/main/java/com/billapp/config/CorsConfig.java
 package com.billapp.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173", "http://192.168.1.11:8080") // your frontend URLs
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "http://localhost:8080",
+                    "https://3nk1g0nq-8080.inc1.devtunnels.ms"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false);
     }
 }
+
--- src/main/java/com/billapp/controller/billappcontroller.java
+++ src/main/java/com/billapp/controller/billappcontroller.java
@@ -3,7 +3,7 @@ package com.billapp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,9 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.billapp.ENTITY.Customer;
 import com.billapp.ENTITY.RentItemPrice;
-import com.billapp.ENTITY.Renteditem;
 import com.billapp.service.CustomerServices;
-
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class billappcontroller {
 	@Autowired
